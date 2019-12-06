67ec22f0640dff86efa074e1dd2ba99231f48565
MObajtek <obajtekmagda@gmail.com>
2019-12-06 15:15:29 +0100

    Add navigation drawer to profile activity

--- app/src/main/java/com/example/ioagh/gamefinder/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/ioagh/gamefinder/ui/profile/ProfileActivity.kt
@@ -1,12 +1,40 @@
 package com.example.ioagh.gamefinder.ui.profile
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.ioagh.gamefinder.R
 import com.example.ioagh.gamefinder.R.*
+
+
 class ProfileActivity : AppCompatActivity() {
+
+    private var drawer: DrawerLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_profile)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar,
+            string.open_navigation_drawer, string.close_navigation_drawer
+        )
+        drawer!!.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onBackPressed() {
+        if (drawer!!.isDrawerOpen(GravityCompat.START)) {
+            drawer!!.closeDrawer(GravityCompat.START)
+        }
+        super.onBackPressed()
     }
 }
